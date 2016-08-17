4fe6b1a545f39bcfe765e8f312d7af36e925b2ca
David Leoni <davidleonigit-github@yahoo.com>
2016-08-17 16:56:37 +0200

    improved javadoc

--- src/main/java/it/unitn/disi/diversicon/DivXmlWriter.java
+++ src/main/java/it/unitn/disi/diversicon/DivXmlWriter.java
@@ -15,14 +15,32 @@ import de.tudarmstadt.ukp.lmf.transform.LMFXmlWriter;
  */
 public class DivXmlWriter extends LMFXmlWriter {
 
+    /**
+     *  Constructs a LMFXmlWriter, XML will be saved to OutputStream out. 
+     *  
+     *  @since 0.1.0
+     */
     public DivXmlWriter(OutputStream outputStream, String dtdPath) throws SAXException {
         super(outputStream, dtdPath);
     }
 
+    /**        
+     * 
+     * Constructs a LMFXmlWriter, XML will be saved to file in outputPath
+     * @param outputPath
+     * @param dtdPath Path of the dtd-File
+     * @throws FileNotFoundException if the writer can not to the specified outputPath     
+     *  @since 0.1.0
+     */
     public DivXmlWriter(String outputPath, String dtdPath) throws FileNotFoundException, SAXException {
         super(outputPath, dtdPath);    
     }
     
+    /** 
+     * {@inheritDoc}
+     * 
+     *  @since 0.1.0
+     */
     @Override
     protected void doWriteElement(Object lmfObject, boolean closeTag) throws SAXException{
             
